e4bfb40c1ac038c6cc4204539a122747b47ae865
Benjamin Ramhorst <bramhorst27@gmail.com>
2022-02-02 23:06:27 +0000

    Added Firebase to project

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
-
+    return 'Hello World! I am the dog you tracked with the amazing sensor from LEG industries...kidding this is just the home page :)'
 
 if __name__ == '__main__':
+    firebase_admin.initialize_app(credentials.Certificate('firebase-key.json'))
     app.run()
