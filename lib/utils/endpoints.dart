31b7ed3234062c3519eeb3df3490f99a7e4259e4
Benjamin Ramhorst <bramhorst27@gmail.com>
2022-02-14 20:22:24 +0000

    Fixed endpoints

--- lib/utils/endpoints.dart
+++ lib/utils/endpoints.dart
@@ -1,7 +1,6 @@
 const String home = "https://leg-barkr.nw.r.appspot.com/";
 const String register = "https://leg-barkr.nw.r.appspot.com/authentication/register";
 const String verify = "https://leg-barkr.nw.r.appspot.com/authentication/verify";
-const String getLastLocation = "https://leg-barkr.nw.r.appspot.com/readings/last/location";
-const String getStepsToday = "https://leg-barkr.nw.r.appspot.com/readings/last/steps";
-
-
+const String getLastLocation = "https://leg-barkr.nw.r.appspot.com/readings/location/last";
+const String getStepsToday = "https://leg-barkr.nw.r.appspot.com/readings/steps/today";
+const String getStepsLastFiveDays = "https://leg-barkr.nw.r.appspot.com/readings/steps/last-five-days";
