84d18b2764f6ad71335490962d67cc3651451a6f
Kacper <kacper.neumann19@imperial.ac.uk>
2022-03-01 09:18:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ accel = lis3dh(bus,2,10)
 
 print("LIS3DH initiated successfully!")
 
+f = open("output.txt","x")
+print("X","Y","Z", file=f)
+f.close()
+
 while True:
     [X,Y,Z] = accel.readAll()
-    print("X: ",X,"\tY: ",Y,"\t Z: ",Z,"\n")
+    with open("output.txt","a") as f:
+        print("X: ",X,"\tY: ",Y,"\t Z: ",Z,"\n")
+        print(X,Y,Z, file=f)
